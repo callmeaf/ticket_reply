77befa69dd82d740964d751d5bd92f205cbba023
callmeaf <alikhoshkar2000@gmail.com>
2025-05-18 11:32:36 +0330

    change mail template

--- resources/views/admin/v1/mail/ticket_replies/replied.blade.php
+++ resources/views/admin/v1/mail/ticket_replies/replied.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
-    # {{ __('callmeaf-ticket-reply::admin_v1.mail.replied.title', ['ref_code' => $ref_code]) }}
+# {{ __('callmeaf-ticket-reply::admin_v1.mail.replied.title', ['ref_code' => $ref_code]) }}
 
-    {{ __('callmeaf-ticket-reply::admin_v1.mail.replied.body', ['title' => $title, 'receiver_identifier' => $receiver_identifier]) }}
+{{ __('callmeaf-ticket-reply::admin_v1.mail.replied.body', ['title' => $title, 'receiver_identifier' => $receiver_identifier]) }}
 
-    @component('mail::button', ['url' => $url])
-        {{ __('callmeaf-ticket-reply::admin_v1.mail.replied.button') }}
-    @endcomponent
+@component('mail::button', ['url' => $url])
+{{ __('callmeaf-ticket-reply::admin_v1.mail.replied.button') }}
+@endcomponent
 
-    {{__('callmeaf-ticket-reply::admin_v1.mail.replied.footer')}}
+{{__('callmeaf-ticket-reply::admin_v1.mail.replied.footer')}}
 
-    {{ config('app.name') }}
+{{ config('app.name') }}
 </x-mail::message>
